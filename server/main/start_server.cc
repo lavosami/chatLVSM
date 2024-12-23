e61b1de6c12860b8ccd5cb68ae2b9f80d83ebe6a
CallCount <maximrabow@gmail.com>
2024-12-23 22:17:44 +0300

    added registration

--- server/main/start_server.cc
+++ server/main/start_server.cc
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]) {
 
     addUser(login, password);
     getUser(login);
-    editUser(login, password);
+    deleteUser(login);
 
     std::string command;
 
@@ -56,7 +56,22 @@ int main(int argc, char* argv[]) {
       } else if (command == "hello") {
         std::cout << "hello." << std::endl;
       } else if (command == "help") {
-        std::cout << "avaliable commands: hello, help, exit" << std::endl;
+        std::cout << "avaliable commands: hello - to print hello." << std::endl
+                  << "help - to see available commands" << std::endl
+                  << "exit - to exit." << std::endl
+                  << "reg - to start registration" << std::endl;
+      } else if (command == "reg") {
+        std::string login, password;
+
+        std::cout << "Login: ";
+        std::getline(std::cin, login);  // Считывает строку, включая пробелы
+        std::cout << "Login - " << login << std::endl;
+
+        std::cout << "Password: ";
+        std::getline(std::cin, password);  // Считывает строку, включая пробелы
+        std::cout << "password - " << password << std::endl;
+
+        addUser(login, password);
       } else {
         std::cout << "command not found: " << command << std::endl;
       }
